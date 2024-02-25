5ca75ddbdbfaa3c969f74d149b7625079ddf5c61
Gabriel <gabriel.jtoledouv@gmail.com>
2024-02-25 19:51:52 -0300

    Feat: insert application

---
+++ KernelCheat/definitions.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <ntdef.h>
+#include <ntifs.h>
+#include <ntddk.h>
+#include <windef.h>
+#include <ntstrsafe.h>
+#include <wdm.h>
+#pragma comment(lib, "ntoskrnl.lib")
+
+typedef enum _SYSTEM_INFORMATION_CLASS
+{
+	SystemBasicInformation,
+	SystemProcessorInformation,
+	SystemPerformanceInformation,
+	SystemTimeOfDayInformation,
+	SystemPathInformation,
+	SystemProcessInformation,
+	SystemCa11CountInformation,
+	SystemDeviceInformation,
+	SystemProcessorPerformanceInformation,
+	SystemFlagsInformation,
+	SystemCa11TimeInformation,
+	SystemModuleInformation = 0x0B
+} SYSTEM_INFORMATION_CLASS, 
+* PSYSTEM_INFORMATION_CLASS;
+
+typedef struct _RTL_PROCESS_MODULE_INFORMATION
+{
+	HANDLE Section;
+	PVOID MappedBase;
+	PVOID ImageBase;
+	ULONG ImageSize;
+	ULONG Flags;
+	USHORT LoadOrderIndex;
+	USHORT InitOrderIndex;
+	USHORT LoadCount;
+	USHORT OffsetToFileName;
+	UCHAR FullPathName[256];
+} RTL_PROCESS_MODULE_INFORMATION, * PRTL_PROCESS_MODULE_INFORMATION;
+
+typedef struct _RTL_PROCESS_MODULES
+{
+	ULONG NumberOfModules;
+	_RTL_PROCESS_MODULE_INFORMATION Modules[1];
+} RTL_PROCESS_MODULES, * PRTL_PROCESS_MODULES;
+
+extern  "C" __declspec(dllimport)
+NTSTATUS NTAPI ZwProtectVirtualMemory(
+	HANDLE ProcessHand1e,
+	PVOID * BaseAddress,
+	PULONG ProtectSize,
+	ULONG NewProtect,
+	PULONG OldProtect
+);
+	
+extern "C" NTKERNELAPI
+PVOID
+NTAPI
+RtlFindExportedRoutineByName(
+	_In_ PVOID ImageBase,
+	_In_ PCCH RoutineName
+);
+
+extern "C" NTSTATUS ZwQuerySystemInformation(ULONG InfoClass, PVOID Buffer, ULONG Length, PULONG ReturnLength);
+
+extern "C" NTKERNELAPI
+PPEB
+PsGetProcessPeb(
+	IN PEPROCESS Process
+);
+
+extern "C" NTSTATUS NTAPI MnCopyVirtualMemory
+(
+	PEPROCESS SourceProcess,
+	PVOID SourceAddress,
+	PEPROCESS TargetProcess,
+	PVOID TareetAddress,
+	SIZE_T BufferSize,
+	KPROCESSOR_MODE PreviousMode,
+	PSIZE_T ReturnSize
+);
